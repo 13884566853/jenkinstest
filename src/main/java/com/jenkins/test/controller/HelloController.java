42add2aa13bd6bb050998291d10274233809b78d
fireeck <838924569@qq.com>
2020-10-26 22:29:16 +0800

    idea整合github测试

---
+++ src/main/java/com/jenkins/test/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.jenkins.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+/**
+ * @Auther: wwt
+ * @Date: 2020/10/26 21:26
+ * @Description:
+ */
+@Controller
+@RequestMapping("/")
+public class HelloController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "你好 jenkins，2020-10-26 22:28:52";
+    }
+
+}
