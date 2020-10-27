5b6e8994be150616969ecc35f30eaa01a77b611e
fireeck <838924569@qq.com>
2020-10-27 23:00:35 +0800

    2020-10-27 22:55:29 加一点情话上去，提升全局变量

--- src/main/java/com/jenkins/test/controller/HelloController.java
+++ src/main/java/com/jenkins/test/controller/HelloController.java
@@ -31,6 +31,7 @@ public class HelloController {
                 "配置了花生壳内网穿透" +
                 "修复webhook添加错误";
     }
+    Random random = new Random(20);
     @RequestMapping("/miss")
     @ResponseBody
     public String miss() {
@@ -38,7 +39,7 @@ public class HelloController {
                 "兽炉沈水bai烟，翠沼残花片，一行行写入相du思传。#天长路远魂飞苦，梦魂不到关山难，长相思，摧心肝。#" +
                 "似此星辰非昨夜，为谁风露立中宵。#人生自是有情痴，此恨不关风与月。#" +
                 "有美人兮，见之不忘，一日不见兮，思之如狂。").split("#");
-        Random random = new Random(20);
+
         int index = random.nextInt(str.length);
         return str[index];
     }
